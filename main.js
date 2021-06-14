e70f45519e4e993042f090999760651f0a37e4e5
Getvion <gog0125@mail.com>
2021-06-14 22:09:05 +0300

    Сделал добавление элемента в список по нажатии клавиши Enter

--- main.js
+++ main.js
@@ -4,29 +4,35 @@ const input = document.querySelector('#input'),
    btn = document.querySelector('#btn'),
    result = document.querySelector('#list');
 
-btn.addEventListener('click', cleanInput);
-
 let todos = [];
-function cleanInput() {
-   let x = input.value;
-   input.value = '';
-   todos.push(x);
 
-   addTodo();
-};
+btn.addEventListener('click', addTodo);
+
+input.addEventListener('keyup', function (event) {
+   if (event.keyCode == 13) {
+      addTodo();
+   }
+});
 
 function addTodo() {
+   let x = input.value;
+   todos.push(x);
    let value = '';
+
    for (const todo of todos) {
       value = todo;
    }
    if (value !== '') {
       result.innerHTML += `<li class="list-item">${value}</li>`;
    }
+   cleanInput();
+};
+
+function cleanInput() {
+   input.value = '';
 };
 
 let removeChildOfList = document.querySelector('#list');
 removeChildOfList.addEventListener('click', function (event) {
    removeChildOfList.removeChild(event.target);
-   console.log(removeChildOfList);
 });
