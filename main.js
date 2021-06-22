9ce6f687873dab277cdbeb0276c7b868065db823
Getvion <gog0125@mail.com>
2021-06-22 16:40:02 +0300

    добавил появление всплывающих окон, срабаотывает при определенном шансе

--- main.js
+++ main.js
@@ -2,37 +2,78 @@
 
 const input = document.querySelector('#input'),
    btn = document.querySelector('#btn'),
-   result = document.querySelector('#list');
+   result = document.querySelector('#list'),
+   rewardTextGood = document.querySelector('.reward--good'),
+   rewardTextBad = document.querySelector('.reward--bad');
+
 
 let todos = [];
 
-btn.addEventListener('click', addTodo);
-
 input.addEventListener('keyup', function (event) {
    if (event.keyCode == 13) {
-      addTodo();
+      doOnClick();
    }
 });
+
+btn.addEventListener('click', doOnClick);
+
+function doOnClick() {
+   addTodo();
+   cleanInput();
+}
 
 function addTodo() {
    let x = input.value;
    todos.push(x);
    let value = '';
-
    for (const todo of todos) {
       value = todo;
    }
    if (value !== '') {
       result.innerHTML += `<li class="list-item">${value}</li>`;
+      giveReward();
    }
-   cleanInput();
-};
-
-function cleanInput() {
-   input.value = '';
-};
+}
 
 let removeChildOfList = document.querySelector('#list');
 removeChildOfList.addEventListener('click', function (event) {
    removeChildOfList.removeChild(event.target);
 });
+
+function cleanInput() {
+   input.value = '';
+}
+
+function giveReward() {
+   const reward = Math.random();
+
+   switch (true) {
+      case (reward <= 0.6):
+         classChangeGood();
+         break;
+
+      case (reward >= 0.9):
+         classChangeBad();
+         break;
+   }
+}
+
+function classChangeGood() {
+   setTimeout(function () {
+      rewardTextGood.classList.add('visible');
+   }, 10);
+   setTimeout(function () {
+      rewardTextGood.classList.remove('visible');
+   }, 2000);
+}
+
+function classChangeBad() {
+   setTimeout(function () {
+      rewardTextBad.classList.add('visible');
+   }, 100);
+
+   setTimeout(function () {
+      rewardTextBad.classList.remove('visible');
+   }, 2000);
+}
+
